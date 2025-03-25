8f66ed3081544de52c948230156d08b24299e8a1
jessK10 <163406443+jessK10@users.noreply.github.com>
2025-03-25 14:46:32 +0100

    Final clean up: ready for Render deployment

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,16 +1,13 @@
 const mongoose = require("mongoose");
 const uniqueValidator = require("mongoose-unique-validator");
 
-const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
-},
-    { timestamps: true } // Adds createdAt & updatedAT fields
-);
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+}, { timestamps: true });
 
-// Apply the plugin
-userSchema.plugin(uniqueValidator, { message: "{PATH} already exists." });
+// ✅Apply the plugin AFTER declaring userSchema
+userSchema.plugin(uniqueValidator, { message: "[PATH] already exists." });
 
-
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("User", userSchema);
